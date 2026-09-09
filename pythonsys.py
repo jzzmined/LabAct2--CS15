e05bc38bc0ed52868844b7e6224ecad2b5f847eb
jzzmined <j.cadelina.554273@umindanao.edu.ph>
2026-09-09 12:53:13 +0700

    adding anothe file

--- pythonsys.py
+++ pythonsys.py
@@ -1,1 +1,20 @@
-class LoadWallet
+class LoadWallet:
+
+    def __init__(self, owner_name,mob_number,start_balance):
+        self.owner_name = owner_name
+        self.mobe_number = mob_number
+        self.start_balance = start_balance
+
+    def top_up(self):
+        print(f"Enter top-up amount: ")
+
+    def send_load(self):
+
+
+    def show_balance(self):
+
+    def send_with_fee(self):
+
+    
+
+
